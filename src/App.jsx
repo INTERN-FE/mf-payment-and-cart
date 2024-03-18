87237278bf753807fe0933f794ac6032bef3b031
aguswesly <agustchannel@gmail.com>
2024-03-18 11:48:46 +0700

    feat : add payment component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,5 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div className="font-bold">Name: mf-ecommerce</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+const App = () => <Payment />;
 ReactDOM.render(<App />, document.getElementById("app"));
