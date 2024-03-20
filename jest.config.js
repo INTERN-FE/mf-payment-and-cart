94ea3b9872d4f544111f26d30ebf9104b2b159b0
xyzuan <xyzuannihboss@gmail.com>
2024-03-20 13:36:12 +0700

    main(test): ignore App.jsx & index.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coveragePathIgnorePatterns: ["/src/App.jsx", "/src/index.js"],
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  verbose: true,
 };
